8c3304c7e1a100d695f12b65979df556981bd020
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-10-09 11:26:06 +0300

    small bug fix

--- blocks/getResourcesByVersion.js
+++ blocks/getResourcesByVersion.js
@@ -4,6 +4,6 @@ module.exports = (req , res  , template) =>{
  
     const {args :{ instance , accessToken}} = req.body;
     //template(req,res,urlEnding,urlParams , instance , accessToken)
-    return template (req, res, null, null,instance , accessToken);
+    return template (req, res, "", null,instance , accessToken);
         
 }
