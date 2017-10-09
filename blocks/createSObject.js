5138d5f31e67fb79b5e5fdcc214aa4fb90c9d116
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-10-09 14:46:00 +0300

    createSobject fixed

--- blocks/createSObject.js
+++ blocks/createSObject.js
@@ -1,8 +1,8 @@
  
-module.exports =(req, res) =>{
-    const {args : {sObjectName  , postBodyjson , instance , accessToken}} = req.body;
+module.exports =(req, res ,template) =>{
+    const {args : {sObjectName  , inputs , instance , accessToken}} = req.body;
 
     // template (req, res,urlEnding,urlParams,postBodyjson , instance , accessToken)
-    return template (req, res, `sobjects/${sObjectName}`,null ,postBodyjson,instance , accessToken);
+    return template (req, res, `sobjects/${sObjectName}`,null ,inputs,instance , accessToken);
    
 }   
