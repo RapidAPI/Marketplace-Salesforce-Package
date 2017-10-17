5f24d1017b7bca1ef1c33f895be913d076c2e31d
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-10-17 14:38:58 +0300

    fix

--- blocks/submitRecordForApproval.js
+++ blocks/submitRecordForApproval.js
@@ -2,7 +2,7 @@
 
 module.exports = (req , res  , template) =>{
 
-    const {args :  {submitRecordForApproval , instance , accessToken}} = req.body;
+    const {args :  {approvalRequest , instance , accessToken}} = req.body;
     // template (req, res,urlEnding,urlParams,postBodyjson , instance , accessToken)
-    return template (req, res, `process/approvals`,null ,submitRecordForApproval, instance , accessToken);
+    return template (req, res, `process/approvals`,null ,approvalRequest, instance , accessToken);
 }
