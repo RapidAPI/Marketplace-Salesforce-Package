fe7005fe55364d61346f2d0bf9ba6b941accb01d
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-10-17 11:37:21 +0300

    fix

--- blocks/deleteObjectRowsByExternalId.js
+++ blocks/deleteObjectRowsByExternalId.js
@@ -4,5 +4,5 @@ module.exports = (req , res  , template) =>{
     
     const {args: {sObjectName ,fieldName ,fieldValue , instance , accessToken}} = req.body;
     // template (req, res, urlEnding,urlParams , instance , accessToken)
-    return template (req, res, `sobjects/PlatformAction/${sObjectName}/${fieldName}/${fieldValue}` , null ,instance , accessToken);
+    return template (req, res, `sobjects/${sObjectName}/${fieldName}/${fieldValue}` , null ,instance , accessToken);
 }
