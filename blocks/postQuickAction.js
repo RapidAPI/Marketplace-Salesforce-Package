c707a11999c158feb188bcfc406da3a3fe76f2d4
noamsalinger1991 <noamsalinger1991@gmail.com>
2017-10-17 15:58:51 +0300

    post fixed

--- blocks/postQuickAction.js
+++ blocks/postQuickAction.js
@@ -2,8 +2,8 @@
 
 module.exports = (req , res  , template) =>{
  
-    const {args :{ actionName, input, instance , accessToken}} = req.body;
+    const {args :{ actionName, inputs, instance , accessToken}} = req.body;
     // template (req, res,urlEnding,urlParams,postBodyjson , instance , accessToken)
-    return template (req, res, `quickActions/${actionName}`, input,instance , accessToken);
+    return template (req, res, `quickActions/${actionName}`, inputs,instance , accessToken);
 
 }
